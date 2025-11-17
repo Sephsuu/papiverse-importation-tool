29868bb290bafbb871b91471f2149daf59ec3e39
Joseph Bataller <batallerjem208@gmail.com>
2025-11-17 21:48:18 +0800

    updated paid list orders

--- project/project/settings.py
+++ project/project/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql', 
-        'NAME': 'papiverse_sales',                     
-        'USER': 'postgres',                    
-        'PASSWORD': 'Pw:424230150119',             
-        'HOST': 'localhost',                        
-        'PORT': '5432',                              
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
